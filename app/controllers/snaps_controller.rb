9f84696953eb07ccf8353d9cc2e7a64257cb69f9
sshariff01 <shoabe.shariff@gmail.com>
2014-08-12 12:30:22 -0400

    Initialize rails project with configured database and base Snaps controller.

---
+++ app/controllers/snaps_controller.rb
@@ -0,0 +1,31 @@
+class SnapsController < ApplicationController
+  require "instagram"
+  
+  # enable :sessions
+  
+  CALLBACK_URL = "http://localhost:3000/redirect/callback"
+  
+  Instagram.configure do |config|
+    config.client_id = "39af25cb82ed4404915821b9448489f3"
+    config.client_secret = "2aa3108232c44ceaa3b8c3ddd571aac0"
+    # For secured endpoints only
+    #config.client_ips = '<Comma separated list of IPs>'
+  end
+  
+  def index
+    if session[:access_token]
+      @access_token = session[:access_token]
+    end
+  end
+  
+  def connect
+    redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+  end
+  
+  def callback
+    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+    session[:access_token] = response.access_token
+    # @access_token = response.access_token
+    render "index"
+  end
+end
