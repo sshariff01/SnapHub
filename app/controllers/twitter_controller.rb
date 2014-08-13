793f2e6c0903566ae72b1ce1b1af371a6c5a6234
sshariff01 <shoabe.shariff@gmail.com>
2014-08-13 12:08:38 -0400

    Fix global twitter configuration.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -4,16 +4,13 @@ class TwitterController < ApplicationController
   
     # Certain methods require authentication. To get your Twitter OAuth credentials,
     # register an app at http://dev.twitter.com/apps
-    Twitter.configure do |config|
+    client = Twitter::REST::Client.new do |config|
       config.consumer_key = 'Dx5V1VzAGrFRfosfH7mn4G6bu'
       config.consumer_secret =  't4XG4rcexsh2bpSVkyar9P2NDvuoUFeteQ2wsjFEIUt6vY0E21'
       config.oauth_token = '2728693344-R0UZ5iJnt31OnuCSyQkyGqAD7wBbyPe8GO68pxI'
       config.oauth_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
     end
-  
-    # Initialize your Twitter client
-    client = Twitter::Client.new
-  
+
     # Post a status update
     client.update("I just posted a status update via the Twitter Ruby Gem !")
     redirect_to request.referer, :notice => 'Tweet successfully posted'
