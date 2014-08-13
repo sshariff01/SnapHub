7ef34c64b8cd53bf4c44e0445d3d6eca9645bf86
sshariff01 <shoabe.shariff@gmail.com>
2014-08-13 13:11:45 -0400

    Use appropriate twitter clients for posting and filtering stream

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -4,18 +4,24 @@ class TwitterController < ApplicationController
   
     # Certain methods require authentication. To get your Twitter OAuth credentials,
     # register an app at http://dev.twitter.com/apps
-    
-    client = Twitter::Streaming::Client.new do |config|
+    client_rest = Twitter::REST::Client.new do |config|
       config.consumer_key = 'Dx5V1VzAGrFRfosfH7mn4G6bu'
       config.consumer_secret =  't4XG4rcexsh2bpSVkyar9P2NDvuoUFeteQ2wsjFEIUt6vY0E21'
       config.access_token = '2728693344-R0UZ5iJnt31OnuCSyQkyGqAD7wBbyPe8GO68pxI'
       config.access_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
     end
     
-    client.update("I'm tweeting with hashtag #testphotographytag2014")
+    client_stream = Twitter::Streaming::Client.new do |config|
+      config.consumer_key = 'Dx5V1VzAGrFRfosfH7mn4G6bu'
+      config.consumer_secret =  't4XG4rcexsh2bpSVkyar9P2NDvuoUFeteQ2wsjFEIUt6vY0E21'
+      config.access_token = '2728693344-R0UZ5iJnt31OnuCSyQkyGqAD7wBbyPe8GO68pxI'
+      config.access_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
+    end
+    
+    client_rest.update("I'm tweeting with hashtag #testphotographytag2014")
     
     topics = ["testphotographytag2014"]
-    client.filter(:track => topics.join(",")) do |object|
+    client_stream.filter(:track => topics.join(",")) do |object|
       puts object.text if object.is_a?(Twitter::Tweet)
     end
     
