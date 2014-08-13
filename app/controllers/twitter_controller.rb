bd84f346715dd026b8e16da9cd9e3edda56ac3d0
sshariff01 <shoabe.shariff@gmail.com>
2014-08-13 18:13:27 -0400

    Make post via twitter. Disable all else.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -11,21 +11,21 @@ class TwitterController < ApplicationController
       config.access_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
     end
     
-    client_rest.update("I'm tweeting with hashtag testphotographytag2014 #testphotographytag2014")
+    client_rest.update("testphotographytag2014 #testphotographytag2014")
     puts 'made a tweet'
     
-    client_stream = Twitter::Streaming::Client.new do |config|
-      config.consumer_key = 'Dx5V1VzAGrFRfosfH7mn4G6bu'
-      config.consumer_secret =  't4XG4rcexsh2bpSVkyar9P2NDvuoUFeteQ2wsjFEIUt6vY0E21'
-      config.access_token = '2728693344-R0UZ5iJnt31OnuCSyQkyGqAD7wBbyPe8GO68pxI'
-      config.access_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
-    end
-    
-    topics = ["testphotographytag2014", "coffee"]
-    
-    client_stream.filter(:track => topics.join(",")) do |object|
-      puts object.text if object.is_a?(Twitter::Tweet)
-    end
+    # client_stream = Twitter::Streaming::Client.new do |config|
+      # config.consumer_key = 'Dx5V1VzAGrFRfosfH7mn4G6bu'
+      # config.consumer_secret =  't4XG4rcexsh2bpSVkyar9P2NDvuoUFeteQ2wsjFEIUt6vY0E21'
+      # config.access_token = '2728693344-R0UZ5iJnt31OnuCSyQkyGqAD7wBbyPe8GO68pxI'
+      # config.access_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
+    # end
+#     
+    # topics = ["coffee"]
+#     
+    # client_stream.filter(:track => topics.join(",")) do |object|
+      # puts object.text if object.is_a?(Twitter::Tweet)
+    # end
     
     # client_rest.search("coffee", :result_type => "recent") do |object|
       # puts 'START'
