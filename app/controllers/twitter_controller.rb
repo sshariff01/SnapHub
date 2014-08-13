b886101fc4935234b9904c586897c0789fca1efa
sshariff01 <shoabe.shariff@gmail.com>
2014-08-13 19:02:46 -0400

    Collect all search results.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -27,7 +27,7 @@ class TwitterController < ApplicationController
       # puts object.text if object.is_a?(Twitter::Tweet)
     # end
     
-    client_rest.search("coffee", :result_type => "recent").take(3).collect do |object|
+    client_rest.search("coffee", :result_type => "recent").collect do |object|
       puts 'START'
       puts object.text
       puts 'FINISH'
