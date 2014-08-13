f60814cd6b50cb969b495682891f5d5e162ad486
sshariff01 <shoabe.shariff@gmail.com>
2014-08-13 19:09:34 -0400

    Try streaming api one more time

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -11,11 +11,27 @@ class TwitterController < ApplicationController
       config.access_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
     end
     
-    client_rest.search("#testphotographytag2014", :result_type => "recent").collect do |object|
-      puts 'START'
-      puts object.text
-      puts 'FINISH'
+    client_rest.update("testphotographytag2014 #testphotographytag2014 new")
+    puts 'made a tweet'
+    
+    client_stream = Twitter::Streaming::Client.new do |config|
+      config.consumer_key = 'Dx5V1VzAGrFRfosfH7mn4G6bu'
+      config.consumer_secret =  't4XG4rcexsh2bpSVkyar9P2NDvuoUFeteQ2wsjFEIUt6vY0E21'
+      config.access_token = '2728693344-R0UZ5iJnt31OnuCSyQkyGqAD7wBbyPe8GO68pxI'
+      config.access_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
     end
+    
+    topics = ["testphotographytag2014"]
+    
+    client_stream.filter(:track => topics.join(",")) do |object|
+      puts object.text if object.is_a?(Twitter::Tweet)
+    end
+    
+    # client_rest.search("#testphotographytag2014", :result_type => "recent").collect do |object|
+      # puts 'START'
+      # puts object.text
+      # puts 'FINISH'
+    # end
     
     render :text => "success"
   end
