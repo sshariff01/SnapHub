72a71087a5d348dccd64803f86f3d44ac2b9bc3c
sshariff01 <shoabe.shariff@gmail.com>
2014-08-13 19:04:46 -0400

    Search for testphotographytag2014

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -27,7 +27,7 @@ class TwitterController < ApplicationController
       # puts object.text if object.is_a?(Twitter::Tweet)
     # end
     
-    client_rest.search("coffee", :result_type => "recent").collect do |object|
+    client_rest.search("testphotographytag2014", :result_type => "recent").collect do |object|
       puts 'START'
       puts object.text
       puts 'FINISH'
