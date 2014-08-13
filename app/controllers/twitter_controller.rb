7ca34aaa3d06e6cef079f14f997de59bc6af2150
sshariff01 <shoabe.shariff@gmail.com>
2014-08-13 12:14:06 -0400

    Replace oauth_token with access_token

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -8,8 +8,8 @@ class TwitterController < ApplicationController
     client = Twitter::Streaming::Client.new do |config|
       config.consumer_key = 'Dx5V1VzAGrFRfosfH7mn4G6bu'
       config.consumer_secret =  't4XG4rcexsh2bpSVkyar9P2NDvuoUFeteQ2wsjFEIUt6vY0E21'
-      config.oauth_token = '2728693344-R0UZ5iJnt31OnuCSyQkyGqAD7wBbyPe8GO68pxI'
-      config.oauth_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
+      config.access_token = '2728693344-R0UZ5iJnt31OnuCSyQkyGqAD7wBbyPe8GO68pxI'
+      config.access_token_secret = 'KdcTYayXEh4zGn6oNNdEvvWtjaM623clrRI07eePQZD8L'
     end
 
     # Post a status update
